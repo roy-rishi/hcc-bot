99712b2055f180be959de033f4810214c2a24435
roy-rishi <72419270+roy-rishi@users.noreply.github.com>
2026-06-21 01:11:58 -0700

    script to send message with button; respond to button interaction with modal; begin to handle modal response

---
+++ worker/scripts/send-message.ts
@@ -0,0 +1,31 @@
+import 'dotenv/config';
+
+// send message with button
+(async () => {
+    const messageResponse = await fetch(
+        `https://discord.com/api/v10/channels/${process.env.DISCORD_CHANNEL_ID}/messages`, {
+        method: "POST",
+        headers: {
+            "Authorization": `Bot ${process.env.DISCORD_BOT_TOKEN}`,
+            "User-Agent": `DiscordBot (rishiroy.com, 1.0.0)`,
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            content: `Welcome to Husky Cycling – we're glad you're here! This community is open to UW students, staff, alumni, and special guests. **Before we can grant you full access to this server, we need to verify your identity**. Please follow one of the following options:\n- **If you have a current UW NetID, select the button below**, and enter your NetID in the pop-up. You will receive an email with instructions to grant you immediate access to this server.\n- If you do not have a current NetID, please email us so we can manually verify you.`,
+            "components": [
+                {
+                    "type": 1,  // action row
+                    "components": [
+                        {
+                            "type": 2,  // button
+                            "custom_id": "startButton",
+                            "label": "Verify with UW NetID",
+                            "style": 1
+                        }
+                    ]
+                }
+            ],
+        })
+    });
+})();
+export{}
--- worker/src/index.ts
+++ worker/src/index.ts
@@ -1,4 +1,5 @@
 /* index.ts: Cloudlfare Worker */
+const nacl = require("tweetnacl");
 
 // environment types (.env)
 export interface Env {
@@ -9,13 +10,37 @@ export interface Env {
     DISCORD_GUILD_ID: string;
     DISCORD_ROLE_ID: string;
     DISCORD_CHANNEL_ID: string;
+    PUBLIC_KEY: string;
     // static files
     ASSETS: Fetcher;
 }
 
+interface Interaction {
+    type: number;
+}
+
+interface ModalSubmission {
+    type: number;
+    member: {
+        user: {
+            id: string;
+            global_name: string;
+        };
+    };
+    data: {
+        components: [
+            {
+                component: {
+                    custom_id: string;
+                    value: string;
+                };
+            }
+        ]
+    };
+}
+
 // endpoints
-const START_PATH = "/start";
-const DISCORD_CALLBACK_PATH = "/discord-callback";
+const INTERACTIONS_PATH = "/interactions";
 
 let redirectReq = function (originUrl: URL, path: "/401" | "/500" | "/404" | "/success"): Response {
     originUrl.pathname = path;
@@ -30,168 +55,98 @@ export default {
         const appOrigin = reqUrl.origin;
         console.log(request.url);
 
-
-        // # GET: Redirects to the Discord OAuth2 page.
-        if (reqUrl.pathname === START_PATH && request.method === "GET") {
-            // callback URL
-            const redirectUrl = new URL(appOrigin);
-            redirectUrl.pathname = DISCORD_CALLBACK_PATH;
-
-            // build Discord auth URL
-            const authUrl = new URL("https://discord.com/oauth2/authorize");
-            authUrl.searchParams.append("client_id", env.DISCORD_CLIENT_ID);
-            authUrl.searchParams.append("response_type", "code");
-            authUrl.searchParams.append("redirect_uri", redirectUrl.toString());
-            authUrl.searchParams.append("scope", "identify");
-
-            // redirect to Discord
-            return Response.redirect(authUrl.toString(), 302);
-        }
-
-
-        // # GET: The callback from Discord OAuth2
-        if (reqUrl.pathname === DISCORD_CALLBACK_PATH && request.method === "GET") {
-            // get Discord OAuth2 auth code
-            const discordCode = reqUrl.searchParams.get("code");
-
-            // throw error if auth code null
-            if (!discordCode) {
-                console.error("Error: Authorization code null");
-                return redirectReq(new URL(appOrigin), "/401");
+        // # Discord interactions endpoint
+        if (reqUrl.pathname === INTERACTIONS_PATH && request.method === "POST") {
+            // get request signature
+            const signature = request.headers.get("X-Signature-Ed25519");
+            const timestamp = request.headers.get("X-Signature-Timestamp");
+            if (!signature || !timestamp) {
+                return new Response("Missing request signature", { status: 401 });
             }
 
-            // exchange authorization code for token
-            const redirectUrl = new URL(appOrigin);
-            redirectUrl.pathname = DISCORD_CALLBACK_PATH;  // not shown to clients
-            const tokenResponse = await fetch("https://discord.com/api/v10/oauth2/token", {
-                method: "POST",
-                body: new URLSearchParams({
-                    client_id: env.DISCORD_CLIENT_ID,
-                    client_secret: env.DISCORD_CLIENT_SECRET,
-                    grant_type: "authorization_code",
-                    code: discordCode,  // the OAuth2 authorization code
-                    redirect_uri: redirectUrl.toString()
-                }),
-                headers: {
-                    "Content-Type": "application/x-www-form-urlencoded",
-                }
-            });
-
-            // return if Discord authorization (token exchange) failed
-            if (!tokenResponse.ok) {
-                // log error
-                console.error(await tokenResponse.text());
-
-                // authorization issue
-                if (tokenResponse.status == 400) {
-                    console.error("Error: The Discord auth code / token exchange failed");
-                    return redirectReq(new URL(appOrigin), "/401");
-                }
-
-                // a different error occured
-                else {
-                    return redirectReq(new URL(appOrigin), "/500");
-                }
-            }
-
-            // parse token response
-            const tokenData = await tokenResponse.json() as {
-                token_type: string;
-                access_token: string;
-                expires_in: number;
-                refresh_token: string;
-                scope: string;
-            }
-
-            // handle bad/unauthorized response body
-            if (tokenData.token_type != "Bearer" || !tokenData.access_token) {
-                return redirectReq(new URL(appOrigin), "/401");
-            }
-
-            // Discord bearer token
-            const token = tokenData.access_token;
-
-            // get user id using token
-            const idResponse = await fetch(
-                "https://discord.com/api/v10/users/@me", {
-                method: "GET",
-                headers: {
-                    "Authorization": `Bearer ${token}`,
-                    "User-Agent": `DiscordBot (${appOrigin}, 1.0.0)`
-                }
-            });
-
-            // handle id response errors
-            if (!idResponse.ok) {
-                console.error(await idResponse.text());
-                if (idResponse.status == 401) {
-                    return redirectReq(new URL(appOrigin), "/401");
-                } else {
-                    return redirectReq(new URL(appOrigin), "/500");
-                }
-            }
-
-            // parse id response
-            const idData = await idResponse.json() as {
-                id: number;
-                username: string;
-            }
-            if (!idData.id) {
-                // return with error if null
-                console.error("Error: could not get Discord member ID");
-                return redirectReq(new URL(appOrigin), "/500");
-            }
-            const memberId = idData.id;
-            console.log(`member id: ${memberId}`);
-
-            // add Discord role
-            const roleResponse = await fetch(
-                `https://discord.com/api/v10/guilds/${env.DISCORD_GUILD_ID}/members/${memberId}/roles/${env.DISCORD_ROLE_ID}`, {
-                method: "PUT",
-                headers: {
-                    "Authorization": `Bot ${env.DISCORD_BOT_TOKEN}`,
-                    "User-Agent": `DiscordBot (${appOrigin}, 1.0.0)`,
-                    "Content-Type": "application/json"
-                }
-            }
+            // verify signature using public key
+            const body = await request.text();
+            const isVerified = nacl.sign.detached.verify(
+                Buffer.from(timestamp + body),
+                Buffer.from(signature, "hex"),
+                Buffer.from(env.PUBLIC_KEY, "hex")
             );
-            if (roleResponse.status != 204) {
-                // an error occurred
-                console.error(await roleResponse.text());
-                // TODO: error handling
+            if (!isVerified) {
+                return new Response("Invalid request signature", { status: 401 })
             }
 
-            // send confirmation message into channel
-            const messageResponse = await fetch(
-                `https://discord.com/api/v10/channels/${env.DISCORD_CHANNEL_ID}/messages`, {
-                method: "POST",
-                headers: {
-                    "Authorization": `Bot ${env.DISCORD_BOT_TOKEN}`,
-                    "User-Agent": `DiscordBot (${appOrigin}, 1.0.0)`,
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify({
-                    content: `You're verified, <@${memberId}>!`
-                })
+            console.log(body);
+            const data = JSON.parse(body)
+
+            // handle ping
+            if (data.type === 1) {
+                return new Response(JSON.stringify({
+                    type: 1
+                }), {
+                    status: 200,
+                    headers: { "Content-Type": "application/json" }
+                });
+            }
+
+            // handle message component (button)
+            if (data.type === 3) {
+                // TODO: check which button was clicked
+                const modalData = {
+                    custom_id: "netIdModal",
+                    title: "Verify with UW NetID",
+                    components: [{
+                        type: 18,  // label
+                        label: "What is your NetID?",
+                        description: "This is the portion before '@uw.edu' in your email address",
+                        component: {
+                            type: 4,  // text input
+                            custom_id: "netId",
+                            style: 1,  // short
+                            placeholder: "NetID"
+                        }
+                    }, {
+                        type: 18,  // label
+                        label: "What is your full name?",
+                        description: "You may enter a preferred name",
+                        component: {
+                            type: 4,  // text input
+                            custom_id: "name",
+                            style: 1,  // short
+                            placeholder: "Dubs II"
+                        }
+                    }]
+                };
+                return new Response(JSON.stringify({
+                    type: 9,  // modal
+                    data: modalData
+                }), {
+                    status: 200, headers: {
+                        "User-Agent": `DiscordBot (${appOrigin}, 1.0.0)`,
+                        "Content-Type": "application/json"
+                    }
+                });
+            }
+
+            // handle modal submission
+            if (data.type === 5) {
+                // TODO: check which modal was submitted
+                // get submission user info
+                const userId = data.member.user.id;
+                const userName = data.member.user.global_name;
+
+                // get form input
+
+                console.log({ userId, userName });
+            }
+
+            return new Response("Unhandled", {
+                status: 500,
             });
-
-            // handle message response errors
-            if (!messageResponse.ok) {
-                // an error occurred
-                console.error(await roleResponse.text());
-                // TODO: error handling
-            }
-
-            // TODO: change nickname
-            // TODO: set/verify cookie
-
-            // redirect to success page
-            return redirectReq(new URL(appOrigin), "/success");
         }
+
 
 
         // # Invalid path (404)
-        // return static html with 404 status
         const htmlUrl = new URL(appOrigin);
         htmlUrl.pathname = "/404.html";
         const htmlResponse = await env.ASSETS.fetch(htmlUrl);
