8434d1e61694675ca9f9b07e07648bc6052562c0
roy-rishi <72419270+roy-rishi@users.noreply.github.com>
2026-08-01 21:33:54 -0700

    minor edits to welcome message; add tsconfig for scripts

--- worker/scripts/send-message.ts
+++ worker/scripts/send-message.ts
@@ -1,10 +1,12 @@
-import 'dotenv/config';
-import { ComponentType, ButtonStyle } from '../src/constants'
+import process from 'node:process'
+import { ComponentType, ButtonStyle } from '../src/constants.ts'
+
+process.loadEnvFile("../.dev.vars");
 
 // send message with button
 (async () => {
     const res = await fetch(
-        `https://discord.com/api/v10/channels/${process.env.DISCORD_CHANNEL_ID}/messages`, {
+        `https://discord.com/api/v10/channels/${process.env.DISCORD_ANNOUNCEMENT_CHANNEL_ID}/messages`, {
         method: "POST",
         headers: {
             "Authorization": `Bot ${process.env.DISCORD_BOT_TOKEN}`,
@@ -12,7 +14,7 @@ import { ComponentType, ButtonStyle } from '../src/constants'
             "Content-Type": "application/json"
         },
         body: JSON.stringify({
-            content: `Welcome to Husky Cycling – we're glad you're here! This community is open to UW students, staff, and alumni. **To access the discussion channels, please verify your identity using one of the following options**:\n- **If you have a current UW NetID**, select *Verify with NetID* and enter your NetID and name in the pop-up. You will receive an email with a link to complete your automatic verification.\n- If you do not have a current NetID, select *Contact Us* to send us an email so we can manually verify you.`,
+            content: `Welcome to the Husky Cycling Discord server! This community is open to UW students, staff, and alumni. **TO ACCESS THE DISCUSSION CHANNELS, please verify your identity using one of the following options**:\n- **If you have a current UW NetID**, select *Verify with NetID* and enter your NetID and name in the pop-up. You will receive an email with a link to complete your automatic verification.\n- If you do not have a current NetID, select *Contact Us* to send us an email so we can manually verify you.`,
             "components": [
                 {
                     "type": ComponentType.ACTION_ROW,
